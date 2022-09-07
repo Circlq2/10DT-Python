ef5c82381799e3089f9d1df6714b3336c883ea9a
Circlq <103474799+Circlq2@users.noreply.github.com>
2022-09-08 10:07:04 +1200

    Add files via upload

---
+++ Lesson2/problem1.py
@@ -0,0 +1,10 @@
+# E G Wisnewski
+# Varible including varibles
+
+# Problem 01
+# This programs is designed to track and total goals scored by RC 2nd XI for each game played.,
+game1_Pats = 7
+game2_Coll = 5
+game3_wellHigh = 10
+teamTotal = game1_Pats + game2_Coll + game3_wellHigh
+print ("Total Goals Scored: ", teamTotal)
---
+++ Lesson2/problem3.py
@@ -0,0 +1,15 @@
+dieselPrice = 1.47
+# Remember varible names cannot begin with a digit
+petrol95Price = 2.17
+petrol91Price = 2.06
+print ("Initial price:")
+print("diesel price $" + str(dieselPrice))
+print("95 price $" + str(petrol95Price))
+print("91 unleashed price $" + str(petrol91Price))
+dieselPrice = dieselPrice * 1.1
+petrol95Price = petrol95Price * 1.1
+petrol91Price = petrol91Price * 1.1
+print("price increase of 10%:")
+print("Diesel price $" + str(dieselPrice))
+print("95 price $" +str(petrol95Price))
+print("91 unleashed price $" +str(petrol91Price))
